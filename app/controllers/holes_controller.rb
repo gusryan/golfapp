5685d48cb817128f2895a9a027958155e338822b
Gus Ryan <guzalrya@msu.edu>
2013-08-12 19:43:31 -0400

    match updates
    cannot show/get discreet handicaps for the match show page

--- app/controllers/holes_controller.rb
+++ app/controllers/holes_controller.rb
@@ -3,7 +3,7 @@ class HolesController < ApplicationController
  def create
  
 # We need to get the course so that Rails knows where to assign the hole
-@course = Course.find(params[:course_id])
+@course = Course.find(params[:id])
  
 # Here we create a new hole based on the course data from our form (params[:hole])
 @hole = @course.holes.create(hole_params)
@@ -11,13 +11,13 @@ class HolesController < ApplicationController
 # Once the course is saved, we redirect the user back to the course they were viewing
 redirect_to course_path(@course)
  
-end
+ end
 
 # This needs to be at the bottom (it may be there, but you'll have to add your params)
 
 # By default, Rails 4 does not trust input from the scary internet.
 # You need to tell it what params (form fields, really) are acceptable
-def hole_params
-params.require(:hole).permit(:hole_number, :par, :handicap, :yardage)
-end
+  def hole_params
+    params.require(:hole).permit(:hole_number, :par, :handicap, :yardage)
+  end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,5 @@
 class Match < ActiveRecord::Base
   belongs_to :course
   belongs_to :golfer
+  has_many :holes
 end
